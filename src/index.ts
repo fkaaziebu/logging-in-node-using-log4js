24dcc751428bd81ebe9ec056e1d41ffef5c86790
fkaaziebu <frederickaziebu1998@gmail.com>
2023-05-07 20:46:08 +0000

    log4js
    Using log4js in node logging

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import { getLogger, configure } from "log4js";
+
+configure({
+  appenders: {
+    app: { type: "file", filename: "app.log" },
+    out: { type: "stdout" },
+    multi: {
+      type: "multiFile",
+      base: "logs/",
+      property: "categoryName",
+      extension: ".log",
+      maxLogSize: 1024,
+      backup: 3,
+      compress: true,
+    },
+  },
+  categories: {
+    default: {
+      appenders: ["app", "out", "multi"],
+      level: "debug",
+    },
+  },
+});
+
+const logger = getLogger();
+
+logger.info("Hello world! my first log4js test");
+logger.debug("Logging debug");
+logger.warn("Logging for warning");
+logger.error("logging error");
